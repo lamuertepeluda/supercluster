5a2842d542a90daf30518352878b4e87f30c8d99
Vito Macchia <vito.macchia@gmail.com>
2017-01-30 11:56:04 +0100

    Update README; Improve demo with worker accepting aggregator from blob

--- demo/index.js
+++ demo/index.js
@@ -12,10 +12,74 @@ var markers = L.geoJson(null, {
     pointToLayer: createClusterIcon
 }).addTo(map);
 
-var worker = new Worker('worker.js');
+// this example assumes the original point has property CATEGORY_NAME
+var CATEGORY_NAME = 'featureclass';
+
+/**
+ * Accumulator function that get executed by supercluster.
+ * @param pointProperties - mandatory: received by supercluster
+ * @param neighborProperties - mandatory: received by supercluster
+ * 
+ * other parameters such as propertyName have to be supplied in accumulatorFunctionArguments
+ */
+function accumulatorFunction(propertyName, pointProperties, neighborProperties) {
+    var retval = {
+        categories: {}
+    };
+    var pointCategory = typeof pointProperties[propertyName] === 'undefined' ? null : pointProperties[propertyName];
+    var neighborCategory = typeof neighborProperties[propertyName] === 'undefined' ? null : neighborProperties[propertyName];
+    pointCategory = pointCategory === '' ? 'no-category' : pointCategory;
+    neighborCategory = neighborCategory === '' ? 'no-category' : neighborCategory;
+
+    var updatedCount = 0;
+    if (pointCategory !== null && neighborCategory !== null) {
+        if (pointCategory === neighborCategory) {
+            retval.categories[pointCategory] = 2;
+        } else {
+            retval.categories[pointCategory] = 1;
+            retval.categories[neighborCategory] = 1;
+        }
+    } else if (pointCategory !== null && neighborCategory === null) {
+        updatedCount = neighborProperties.categories.hasOwnProperty(pointCategory) ? (neighborProperties.categories[pointCategory] + 1) : 1;
+        // console.log('UC ' + pointCategory, updatedCount);
+        retval.categories = Object.assign({}, neighborProperties.categories, {
+            [pointCategory]: updatedCount
+        });
+    } else if (pointCategory === null && neighborCategory !== null) {
+        updatedCount = pointProperties.categories.hasOwnProperty(neighborCategory) ? pointProperties.categories[neighborCategory] + 1 : 1;
+        // console.log('CU ' + neighborCategory, updatedCount);
+        retval.categories = Object.assign({}, pointProperties.categories, {
+            [neighborCategory]: updatedCount
+        });
+    } else {
+        //First merge
+        retval.categories = Object.assign({}, neighborProperties.categories, pointProperties.categories);
+        //Then sum common properties
+        for (var c of Object.keys(retval.categories)) {
+            if (neighborProperties.categories.hasOwnProperty(c) && !pointProperties.categories.hasOwnProperty(c)) {
+                retval.categories[c] = neighborProperties.categories[c];
+            } else if (pointProperties.categories.hasOwnProperty(c) && !neighborProperties.categories.hasOwnProperty(c)) {
+                retval.categories[c] = pointProperties.categories[c];
+            } else if (pointProperties.categories.hasOwnProperty(c) && neighborProperties.categories.hasOwnProperty(c)) {
+                retval.categories[c] = pointProperties.categories[c] + neighborProperties.categories[c];
+            }
+
+        }
+
+    }
+    return retval;
+}
+
+//See worker.js
+var worker = getSuperclusterWorker();
+
+worker.onerror = function(e) {
+    console.error('Worker error', e);
+};
+
 var ready = false;
 
-worker.onmessage = function (e) {
+worker.onmessage = function(e) {
     if (e.data.ready) {
         ready = true;
         update();
@@ -24,6 +88,25 @@ worker.onmessage = function (e) {
         markers.addData(e.data);
     }
 };
+
+/**
+ * - superclusterOptions: options for the supercluster (mandatory)
+ * - accumulatorFunction: function([optional_arguments], pointProperties, neighborProperties) - must be provided
+ * in order to get the custom property stats for each cluster. Can be defined in the main thread
+ * - accumulatorFunctionArguments: optional array of arguments for accumulatorFunction (see optional_arguments above)
+ */
+worker.postMessage({
+    superclusterOptions: {
+        maxZoom: 17,
+        extent: 256,
+        radius: 60,
+        log: true
+    },
+    accumulatorFunction: new Blob([accumulatorFunction.toString()], {
+        type: 'application/javascript'
+    }),
+    accumulatorFunctionArguments: [CATEGORY_NAME]
+});
 
 function update() {
     if (!ready) return;
@@ -39,8 +122,6 @@ map.on('moveend', update);
 function sum(p, n) {
     return p + n;
 }
-
-var CATEGORY_NAME = 'featureclass'; //must be the same property used in aggregator.js
 
 function onMarkerClick(e) {
     var props = e.target.feature.properties;
@@ -59,7 +140,7 @@ function onMarkerClick(e) {
 function getClusterText(categories, total) {
     var categoryNames = Object.keys(categories);
 
-    var getTooltipHtml = function (htmlString, categoryName) {
+    var getTooltipHtml = function(htmlString, categoryName) {
         var percent = (100 * categories[categoryName] / total).toFixed(2);
         var line = '<span>' + categoryName + '</span>: ' + percent + '%';
         htmlString += line + '<br>';
--- demo/worker.js
+++ demo/worker.js
@@ -1,43 +1,70 @@
 'use strict';
 
-importScripts('../dist/supercluster.js');
-importScripts('./aggregator.js');
-
-var now = Date.now();
-
-var index;
-
-getJSON('../test/fixtures/places.json', function (geojson) {
-    console.log('loaded ' + geojson.length + ' points JSON in ' + ((Date.now() - now) / 1000) + 's');
-
-    index = supercluster({
-        log: true,
-        radius: 60,
-        extent: 256,
-        maxZoom: 17,
-        accumulator: aggregatorFunction || null
-    }).load(geojson.features);
-
-    console.log(index.getTile(0, 0, 0));
-
-    postMessage({ready: true});
-});
-
-self.onmessage = function (e) {
-    if (e.data) {
-        postMessage(index.getClusters(e.data.bbox, e.data.zoom));
+//origin = .. won't work in a blob
+function workerBodyDefinition(origin) {
+    try {
+        importScripts(origin + '/dist/supercluster.js');
+    } catch (ex) {
+        console.warn(ex);
+        self.terminate();
     }
-};
 
-function getJSON(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.onload = function () {
-        if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300 && xhr.response) {
-            callback(xhr.response);
+    var now = Date.now();
+
+    var index;
+
+    function loadData(superclusterOptions) {
+        getJSON(origin + '/test/fixtures/places.json', function(geojson) {
+            console.log('loaded ' + geojson.length + ' points JSON in ' + ((Date.now() - now) / 1000) + 's');
+
+            index = supercluster(superclusterOptions).load(geojson.features);
+
+            console.log(index.getTile(0, 0, 0));
+            postMessage({
+                ready: true
+            });
+        });
+    }
+
+    self.onmessage = function(e) {
+        var data = e.data;
+        if (data.superclusterOptions) {
+            var superclusterOptions = data.superclusterOptions;
+            if (data.accumulatorFunction instanceof Blob) {
+                importScripts(URL.createObjectURL(data.accumulatorFunction));
+                if (Array.isArray(data.accumulatorFunctionArguments)) {
+                    superclusterOptions['accumulator'] = accumulatorFunction.bind.apply(accumulatorFunction, [null].concat(data.accumulatorFunctionArguments));
+                } else {
+                    superclusterOptions['accumulator'] = accumulatorFunction; //loaded from blob
+                }
+            }
+            loadData(superclusterOptions);
+        }
+        if (data.bbox && data.zoom) {
+            postMessage(index.getClusters(data.bbox, data.zoom));
         }
     };
-    xhr.send();
+
+    function getJSON(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.onload = function() {
+            if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300 && xhr.response) {
+                callback(xhr.response);
+            }
+        };
+        xhr.send();
+    }
+}
+
+function getSuperclusterWorker() {
+    var workerBodyCode = '(' + workerBodyDefinition.toString() + ')("' + window.location.origin + '")';
+
+    var workerBody = URL.createObjectURL(new Blob([workerBodyCode], {
+        type: 'application/javascript'
+    }));
+
+    return new Worker(workerBody);
 }
